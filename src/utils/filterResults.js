8e7e619ba40a3f2f363744b35023da69d837191b
DoubleDebug <duledodicc@gmail.com>
2022-05-23 16:16:22 +0200

    chore: removed unnecessary filter condition

--- src/utils/filterResults.js
+++ src/utils/filterResults.js
@@ -1,7 +1,9 @@
 export function filterResults(query, results) {
     const regex = new RegExp(query, 'i');
-    const filtered = results
-        .filter((r) => r !== '') // filter empty results
-        .filter((r) => regex.test(r)); // remove result metadata (movie info, book info, etc)
-    return Array.from(new Set(filtered)); // remove duplicates
+    
+    // remove all results that don't contain the search query
+    const filtered = results.filter((r) => regex.test(r));
+    
+    // remove all duplicates
+    return Array.from(new Set(filtered));
 }
